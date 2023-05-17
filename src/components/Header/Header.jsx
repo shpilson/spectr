5736c5f9b27be3d4fb38a84769af43a09f87d8a6
pavel11123 <korobko.11@inbox.ru>
2023-05-17 23:25:55 +0600

    animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,20 +5,44 @@ import { Logo } from "../Logo/Logo";
 import { NavigationMenu } from "../NavigationMenu/NavigationMenu";
 import { ButtonTheme } from "../ButtonTheme/ButtonTheme";
 import { ButtonLanguage } from "../ButtonLanguage/ButtonLanguage";
+import { motion } from "framer-motion";
+
+const headerAnimation = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+    ease: "easeOut",
+  },
+  visible: (custom) => ({
+    y: 0,
+    ease: "easeOut",
+    opacity: 1,
+    transition: { duration: 0.5, delay: custom * 0.3 },
+  }),
+};
 
 const Header = () => {
   return (
     <>
-      <header className={cn(s.header)}>
-        <div className={cn(s.wrapper, "d-fl")}>
+      <motion.header
+        className={cn(s.header)}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
+        <motion.div
+          className={cn(s.wrapper, "d-fl")}
+          variants={headerAnimation}
+          custom={2}
+        >
           <Logo />
           <NavigationMenu />
           <div className={cn(s.switchButton, "d-fl")}>
             <ButtonTheme />
             <ButtonLanguage />
           </div>
-        </div>
-      </header>
+        </motion.div>
+      </motion.header>
     </>
   );
 };
